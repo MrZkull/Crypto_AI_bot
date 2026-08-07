34413998bb4d2bd5797d790fdf3bd477c6fc37fa
MrZkull <166250462+MrZkull@users.noreply.github.com>
2026-08-07 18:04:58 +0530

    Update deribit_client.py

--- deribit_client.py
+++ deribit_client.py
@@ -251,7 +251,7 @@ class DeribitClient:
     def split_amount(self, symbol: str, total) -> tuple:
         if total <= 0: return 0, 0
         step  = self.get_min_trade_amount(symbol)
-        tp1   = max(step, math.floor(total / 2.0 / step) * step)
+        tp1   = max(step, math.floor((total * 0.60) / step) * step) # 👈 Multiplying by 0.60 = 60%
         tp2   = total - tp1
         if tp2 < step: tp1 = total; tp2 = 0
         decimals = _calc_decimals(step)
